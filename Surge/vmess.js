a13111eccc0a27855b74fe72b0c32aabd735cc85
Mr.She <78968666+onlynair@users.noreply.github.com>
2023-02-11 09:37:37 +0000

    Update vmess.js

--- Surge/vmess.js
+++ Surge/vmess.js
@@ -2,7 +2,7 @@
 async function operator(proxies = []) {
     const _ = lodash
     return proxies.map((p = {}) => {
-        _.set(p, 'ws-opts.headers.Host', 'api.cloud.189.cn') // 改混淆
+        _.set(p, 'ws-opts.headers.Host', 'cloud189-shzh-person.oos-gdsz.ctyunapi.cn') // 改混淆
         return p
     })
 }
