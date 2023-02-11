4e4e4ec60d616b5c74a00b13edb76c9203595611
Mr.She <78968666+onlynair@users.noreply.github.com>
2023-02-11 09:34:39 +0000

    Update trojan.js

--- Surge/trojan.js
+++ Surge/trojan.js
@@ -3,7 +3,7 @@ async function operator(proxies = []) {
     const _ = lodash
     return proxies.map((p = {}) => {
         _.set(p, 'skip-cert-verify', true)  // 改跳过证书验证
-        _.set(p, 'sni', 'api.cloud.189.cn') // 改混淆
+        _.set(p, 'sni', 'cloud189-shzh-person.oos-gdsz.ctyunapi.cn') // 改混淆
         return p
     })
 }
